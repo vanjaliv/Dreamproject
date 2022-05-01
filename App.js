a8c3b1dcea4a628f8aba1eb7c3d73cd7064d418e
vanjaliv <96474534+vanjaliv@users.noreply.github.com>
2022-05-01 18:19:03 +0530

    dreamprojectreact

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+/*import './App.css';
+import Hero from './hero';
+import Shaktiman from './shaktiman';
+import Mysample from './sample';
+import Mygame from './Mygame';
+import Home from './home';*/
+import Dream from './Dream';
+function App() {
+  return (
+    <div>
+    <Dream />
+      </div>
+);
+}
+
+export default App;
---
+++ Dream.js
@@ -0,0 +1,18 @@
+import React from "react";
+function Dream() {
+    return (
+        <div>
+         <h1 data-ns-test="project-name"> project-name</h1>
+         <h2> computer center startup</h2>
+         <div>
+             <h2 data-ns-test="project-description">project-description</h2>
+         <p>A young mind is the sharpest mind. It learns quick and acts quicker.
+              The education system today focuses on books and rote-learning, 
+              but times are changing as these young geniuses,
+               who chose to take a different path, have proved. 
+               They have picked machines over books and ideas over words.</p>
+          </div>
+        </div>
+    );
+}
+export default Dream
