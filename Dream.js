72210d70536d81d755185c90975a91425b5773f9
vanjaliv <96474534+vanjaliv@users.noreply.github.com>
2022-05-02 11:48:06 +0530

    Update Dream.js

--- Dream.js
+++ Dream.js
@@ -1,16 +1,17 @@
 import React from "react";
 function Dream() {
     return (
-        <div>
-         <h1 data-ns-test="project-name"> project-name</h1>
+        <div data-ns-test="project-name">
+             <h1>"project-name"</h1>
          <h2> computer center startup</h2>
-         <div>
-             <h2 data-ns-test="project-description">project-description</h2>
-         <p>A young mind is the sharpest mind. It learns quick and acts quicker.
+         <div data-ns-test="project-description">
+             <h2>project-description</h2>
+              A young mind is the sharpest mind. It learns quick and acts quicker.
               The education system today focuses on books and rote-learning, 
               but times are changing as these young geniuses,
                who chose to take a different path, have proved. 
-               They have picked machines over books and ideas over words.</p>
+              They have picked machines over books and ideas over words.
+            
           </div>
         </div>
     );
