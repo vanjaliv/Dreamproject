df7e23344364dd287c4d032787932b7e89a80f3e
vanjaliv <96474534+vanjaliv@users.noreply.github.com>
2022-05-02 11:06:13 +0530

    Update Dream.js

--- Dream.js
+++ Dream.js
@@ -5,7 +5,7 @@ function Dream() {
          <h1 data-ns-test="project-name"> project-name</h1>
          <h2> computer center startup</h2>
          <div>
-             <h2 data-ns-test="project-description">project-description</h2>
+             <data-ns-test="project-description">project-description>
          <p>A young mind is the sharpest mind. It learns quick and acts quicker.
               The education system today focuses on books and rote-learning, 
               but times are changing as these young geniuses,
